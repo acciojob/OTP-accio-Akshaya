ff9cf789c512d21ef874a53b12745f15787ceef9
accio-Akshaya <akshayaganesh0910@gmail.com>
2025-05-08 09:49:16 -0700

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+const inputs = document.querySelectorAll(".code");
+
+inputs.forEach((input, index) =>{
+	input.addEventListener("input",(e) => {
+		const value = e.target.value;
+	})
+})
