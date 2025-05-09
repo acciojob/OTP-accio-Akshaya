758fa5a703fd5dbba6e4ea881580ea78c963419e
accio-Akshaya <akshayaganesh0910@gmail.com>
2025-05-08 23:36:50 -0700

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,35 @@
 //your JS code here. If required.
 const inputs = document.querySelectorAll(".code");
 
+window.addEventListener("load",()=>{
+	inputs[0].focus();
+});
+
 inputs.forEach((input, index) =>{
-	input.addEventListener("input",(e) => {
+	input.addEventListener("input", (e) = >{
 		const value = e.target.value;
-	})
+	if(value.length > 1){
+	 input.value = value.charAt(0);
+	}
+
+	if(value && index < inputs.length - 1){
+		inputs[index + 1].focus();
+	}
+});
+
+input.addEventListener("keydown", (e) => {
+    if (e.key === "Backspace") {
+      if (input.value === "") {
+        if (index > 0) {
+          inputs[index - 1].focus();
+          inputs[index - 1].value = "";
+        }
+      } else {
+        input.value = "";
+      }
+    } else if (e.key.length === 1 && !e.key.match(/[0-9]/)) {
+      e.preventDefault(); // block non-numeric keys
+    }
+  });
+});
 })
